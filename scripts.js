d54a88d3583f2f76571ff9d3f1fd9fd1f00b9d4a
LAFisherGIS <92408705+LAFisherGIS@users.noreply.github.com>
2021-11-13 16:36:02 -0800

    Semi-finalized map

--- scripts.js
+++ scripts.js
@@ -16,12 +16,15 @@ function modeSet(){
 // End Tero-based scripting
 
 function mapping(){
-  var map = L.map('map').setView([47.25, -122.44], 11);
+  var map = L.map('map', {
+    maxBounds : [[47.42692809746929, -122.85538888556895], [46.61560176506292, -121.11561841555328]],
+    minZoom : 10
+  }).setView([47.08249470427265, -122.21075388911457], 10);
 
   L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/256/{z}/{x}/{y}?access_token={accessToken}', {
       attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
       maxZoom: 18,
-      id: 'mapbox/streets-v11',
+      id: 'lafishergis/ckvwv1qz354hp14s8pcasgvi8',
       accessToken: 'sk.eyJ1IjoibGFmaXNoZXJnaXMiLCJhIjoiY2t2OXJ4dnV1YTY2ZjJwbnpjM3BxbWRnYiJ9.CW4oaT94TkbelBF0Fj4rJw',
   }).addTo(map);
 
@@ -34,7 +37,6 @@ function mapping(){
             d > 2.964255  ? '#fdae61' :
                             '#d7191c';
   }
-
 
   function style(feature) {
       return {
@@ -102,24 +104,11 @@ function mapping(){
     onEachFeature: onEachFeature
   }).addTo(map);
 
-  var legend = L.control({position: 'bottomright'});
+  var Legend =  new L.Control.Legend({
+      position: 'bottomright',
+  });
 
-  legend.onAdd = function (map) {
-
-    var div = L.DomUtil.create('div', 'info legend'),
-      grades = [0, 2.964255, 3.877140, 4.930006, 6.693878],
-      labels = [];
-
-      // loop through our density intervals and generate a label with a colored square for each interval
-    for (var i = 0; i < grades.length; i++) {
-        div.innerHTML +=
-            '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
-            grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '%' + '<br>' : '%+');
-    }
-
-    return div;
-  };
-
-  legend.addTo(map);
+  map.addControl(Legend);
+  $(".legend-container").append( $("#legend") );
 
 }
